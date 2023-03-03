68371b81679e2b3eb3acd5575f89a0e28ecc0399
danricardo88 <daniel.ricardo88@outlook.com>
2023-03-02 21:21:39 -0300

    Finalizado, que surra, que desafio bom.

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { getPokemonImage, getPokemonNumber, Pokemon } from '../../_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
-  styleUrls: ['./pokemon-card.component.sass']
+  styleUrls: ['./pokemon-card.component.sass'],
 })
 export class PokemonCardComponent {
+  @Input()
+  public pokemon!: Pokemon;
 
+  public getPokemonImage = getPokemonImage;
+
+  public getPokemonNumber = getPokemonNumber;
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { PokemonService } from '../../_services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.sass']
+  styleUrls: ['./pokemon-list.component.sass'],
 })
 export class PokemonListComponent {
 
+  constructor(
+    public pokemonService: PokemonService,
+  ) {
+  }
 }
